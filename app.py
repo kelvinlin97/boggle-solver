b28cde958ea645b7a5bdf138e5f93dfb3e6298c2
Kelvin Lin <kelvindlin97@gmail.com>
2024-05-28 18:18:12 -0400

    minor styling changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 from solve_board import solve
 from utils import validate_board
 
@@ -10,6 +10,7 @@ def index():
 
 @app.route('/solve', methods=['POST'])
 def solve_board():
+<<<<<<< Updated upstream
     data = request.get_json()
     board = data.get('board')
     validate_board(board)
@@ -19,6 +20,17 @@ def solve_board():
         return jsonify({"error": "Invalid board: Board must contain only lowercase letters"}), 400
     solve(board)
     return "hi'"
+=======
+    try:
+        data = request.form
+        board = format_user_input(data)
+        validate_board(board)
+        word_list = solve(board)
+        return render_template('index.html', response=word_list)
+    except Exception as e:
+        error_message = str(e)
+        return render_template('index.html', error_message=error_message)
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app.run(debug=True)
