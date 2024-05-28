4a3e90051900ef56869fc4cef08542e2402ae1f3
Kelvin Lin <kelvindlin97@gmail.com>
2024-05-28 12:03:26 -0400

    fixed bug with game logic by passing copy of string

--- data/utils.py
+++ data/utils.py
@@ -1,7 +1,11 @@
+import os
+
 def clean_word_set(filename):
     valid_words = []
     try:
-        with open(filename, 'r') as file:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir, filename)
+        with open(file_path, 'r') as file:
             for word in file:
                 word = word.strip()
                 if len(word) >= 3:
@@ -11,6 +15,7 @@ def clean_word_set(filename):
     
     return valid_words
 
+LATEST_WORD_SET = clean_word_set('words_alpha.txt')
+
 def validate_word(word):
-    word_set = clean_word_set('words_alpha.txt')
-    return word in word_set
+    return word in LATEST_WORD_SET
--- solve_board.py
+++ solve_board.py
@@ -2,36 +2,33 @@ from data.utils import validate_word
 
 # TODO: validate board
 
-placeholder = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'] ['i', 'j', 'k', 'l'], ['m', 'n', 'o', 'p']]
+placeholder = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], ['i', 'j', 'k', 'l'], ['m', 'n', 'o', 'p']]
 
 def solve(board):
     # dfs board
         # explore path 
             # only visit node if node has not been visited already in current run (do not want to repeat)
             # once valid path (3 or more)
-            # check if word has been seen already (found in another dfs call)
-                # if not seen, check if word is valid (from set of valid words)
-                    # if valid, add word to seen
+                # check if word has been seen already (found in another dfs call)
+                    # if not seen, check if word is valid (from set of valid words)
+                        # if valid, add word to seen
     
-    valid_words = []
+    valid_words = set()
+
+    def dfs(x, y, marked, prev):  
+        marked.add((x, y))
+        cur = prev + board[x][y]    
+        if len(cur) >= 3 and validate_word(cur):
+            valid_words.add(cur)
+        for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x - 1, y - 1), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1)):
+            if 0 <= dx < len(board) and 0 <= dy < len(board) and (dx, dy) not in marked:
+                dfs(dx, dy, marked.copy(), cur)
     
     for i in range(len(board)):
         for j in range(len(board[i])):
-            visited = set()
-            visited.add((i, j))
-            dfs(i, j, visited, '')
-
-        
-    def dfs(x, y, marked, cur):  
-        cur += board[x][y]
-        if len(cur) >= 3 and validate_word(cur):
-            valid_words.append(cur)
-        for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x - 1, y - 1), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1)):
-            if (dx, dy) not in marked and 0 <= dx < len(board) and 0 <= dy < len(board):
-                marked.add((dx, dy))
-                dfs(dx, dy, marked, cur)
-
-    return valid_words
+            dfs(i, j, set(), '')
+    
+    return list(valid_words)
     
 
 res = solve(placeholder)
